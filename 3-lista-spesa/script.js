c9535830d178bedaf9aafc6ef5216b07c96974a5
henri-kapidani <h.kapidani@gmail.com>
2023-04-20 11:50:53 +0400

    add solution with while and button

--- 3-lista-spesa/script.js
+++ 3-lista-spesa/script.js
@@ -18,9 +18,37 @@ const arrList = [
 const eleList = document.querySelector('#list');
 
 // soluzione con il ciclo for
-for (let i = 0; i < arrList.length; i++) {
-	console.log(arrList[i]);
-	eleList.innerHTML += `<li>${arrList[i]}</li>`;
-}
+// for (let i = 0; i < arrList.length; i++) {
+// 	console.log(arrList[i]);
+// 	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+// }
 
 // soluzione con il ciclo while
+let i = 0;
+while (i < arrList.length) {
+	console.log(arrList[i]);
+	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+	i++;
+}
+
+const btnAdd = document.querySelector('#btn-add');
+const inputAdd = document.querySelector('#input-add');
+
+btnAdd.addEventListener('click',
+	function () {
+		// prendere il valore dall'input
+		const addText = inputAdd.value.trim().toLowerCase();
+
+		if (addText != '') {
+			// console.log di addText
+			console.log(addText);
+			// pusharlo nell'array
+			arrList.push(addText);
+			console.log(arrList);
+			// stampare il valore in pagina
+			eleList.innerHTML += `<li>${addText}</li>`;
+		}
+
+		inputAdd.value = '';
+	}
+)
